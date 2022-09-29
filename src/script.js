ffb279ed7d939837c6f269d652e16b7e0eab9478
Kyle Langille <kyle.a.langille@gmail.com>
2022-09-29 20:05:52 -0230

    minor tweaks

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 "use strict";
 const overlay = document.querySelector(".overlay");
-const addReadBookBtn = document.querySelector(".add-read-book--btn");
+const addReadBookBtn = document.getElementById("add-read-book--btn");
 const addUnreadBookBtn = document.querySelector(".add-unread-book--btn");
 const addBookForm = document.querySelector(".add-read-book--form");
 const closeFormBtn = document.querySelector(".form-close--btn");
@@ -16,15 +16,53 @@ class ReadLibrary {
   constructor() {
     this.readBooks = [];
   }
+
+  addReadBook(newReadBook) {
+    if (!this.isInReadLibrary(newReadBook)) {
+      readBooks.push(newReadBook);
+    }
+  }
+
+  removeReadBook(title) {
+    this.readBooks = this.readBooks.filter((book) => book.title !== title);
+  }
+
+  getReadBook(title) {
+    return this.readBooks.find((book) => book.title === title);
+  }
+
+  isInReadLibrary(newReadBook) {
+    return this.readBooks.some((book) => book.title === newReadBook.title);
+  }
 }
+
+const readLibrary = new ReadLibrary();
 
 class UnreadLibrary {
   constructor() {
     this.unreadBooks = [];
   }
+
+  addUnreadBook(newUnreadBook) {
+    if (!this.isInUnreadLibrary(newUnreadBook)) {
+      unreadBooks.push(newUnreadBook);
+    }
+  }
+
+  removeUnreadBook(title) {
+    this.unreadBooks = this.unreadBooks.filter((book) => book.title !== title);
+  }
+
+  getUnreadBook(title) {
+    return this.unreadBooks.find((book) => book.title === title);
+  }
+
+  isInUnreadLibrary(newUnreadBook) {
+    return this.unreadBooks.come((book) => book.title === newUnreadBook.title);
+  }
 }
 
-function addBookToReadLibrary() {}
+const unreadLibrary = new UnreadLibrary();
 
 addReadBookBtn.addEventListener("click", function () {
   addBookForm.classList.add("active");
